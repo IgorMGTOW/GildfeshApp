afd1ca4cdef8dc8755635e06a7606c2a76697d77
Igor MGTOW <96206920+IgorMGTOW@users.noreply.github.com>
2023-04-28 12:09:56 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int Gildfesh1 = 1;
+    void *p = calloc((size_t) 10, (size_t) 99999);
+    printf("Welcome to Gildfesh test application!\n");
+    while (Gildfesh1 =1) {
+        printf("Gildfesh is a member of the staff team in Save Minecraft Server");
+    }
+    free(p);
+    return 0;
+}
