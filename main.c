c05beffc54faf77261bd57a80788a9f293f6fce5
Igor MGTOW <96206920+IgorMGTOW@users.noreply.github.com>
2023-05-01 23:13:35 +0200

    Update main.c
    changed it so it says "Discord Server" instead of just "Server"

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main()
     void *p = calloc((size_t) 10, (size_t) 99999); // allocation of memory
     printf("Welcome to Gildfesh test application!\n");  // first printf statement
     while (Gildfesh1 =1) {  // start of while loop
-        printf("Gildfesh is a member of the staff team in Save Minecraft Server\n");
+        printf("Gildfesh is a member of the staff team in Save Minecraft Discord Server\n");
     }
     free(p); // end of memory allocation
     return 0;
