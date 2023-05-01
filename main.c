0f6d7cdd32169006e05db3394cbd5e8efceace3f
Igor MGTOW <96206920+IgorMGTOW@users.noreply.github.com>
2023-05-01 19:30:44 +0200

    Update main.c
    this changes the code by adding \n to the printf statement

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main()
     void *p = calloc((size_t) 10, (size_t) 99999);
     printf("Welcome to Gildfesh test application!\n");
     while (Gildfesh1 =1) {
-        printf("Gildfesh is a member of the staff team in Save Minecraft Server");
+        printf("Gildfesh is a member of the staff team in Save Minecraft Server\n");
     }
     free(p);
     return 0;
