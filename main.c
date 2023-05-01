65af34840c2596a78b9871b5e5df674822b06155
Igor MGTOW <96206920+IgorMGTOW@users.noreply.github.com>
2023-05-01 21:13:11 +0200

    Update main.c
    i commented on the application

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
+#include <stdio.h>  // includes the important libraries ect
 #include <stdlib.h>
 
 int main()
 {
-    int Gildfesh1 = 1;
-    void *p = calloc((size_t) 10, (size_t) 99999);
-    printf("Welcome to Gildfesh test application!\n");
-    while (Gildfesh1 =1) {
+    int Gildfesh1 = 1;   // start of the variable
+    void *p = calloc((size_t) 10, (size_t) 99999); // allocation of memory
+    printf("Welcome to Gildfesh test application!\n");  // first printf statement
+    while (Gildfesh1 =1) {  // start of while loop
         printf("Gildfesh is a member of the staff team in Save Minecraft Server\n");
     }
-    free(p);
+    free(p); // end of memory allocation
     return 0;
 }
